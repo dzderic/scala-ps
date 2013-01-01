7058c9ae316fb4a328d1243c781b779ede716163
David Zderic <dzderic@gmail.com>
2013-01-01 17:09:00 +1100

    Added an implementation of `executable' and `cwd'

--- src/main/scala/com/dzderic/ps/LinuxProcess.scala
+++ src/main/scala/com/dzderic/ps/LinuxProcess.scala
@@ -4,17 +4,21 @@ import java.nio.file.{Files, Paths}
 import java.nio.file.attribute.PosixFileAttributes
 
 class LinuxProcess(val pid: Long) extends Process {
+  // Path pointing to /proc/<pid>
   private val proc_path = Paths.get(LinuxProcessFactory.PROC_MOUNT, pid.toString)
 
+  // Get the process user/group from the /proc/<pid> directory permissions
   lazy private val attributes = {
     Files.readAttributes[PosixFileAttributes](proc_path, classOf[PosixFileAttributes])
   }
   lazy val user = attributes.owner()
   lazy val group = attributes.group()
 
-  lazy val cmdline = Util.readEntireFile(proc_path.resolve("cmdline").toFile)
+  // `cmdline' is split by null's. Change them to spaces.
+  lazy val cmdline = Util.readEntireFile(proc_path.resolve("cmdline").toFile).replace('\0', ' ').trim
 
-  // TODO: These
-  lazy val executable = null
-  lazy val cwd = null
+  // `exe' and `cwd' are symlinks which are only readable by the same user
+  // running the process, which is why they're Option's
+  lazy val executable = Util.maybeReadProtectedLink(proc_path.resolve("exe").toFile)
+  lazy val cwd = Util.maybeReadProtectedLink(proc_path.resolve("cwd").toFile)
 }
--- src/main/scala/com/dzderic/ps/Util.scala
+++ src/main/scala/com/dzderic/ps/Util.scala
@@ -9,4 +9,15 @@ object Util {
     source.close()
     contents
   }
+
+  def maybeReadProtectedLink(link: File): Option[String] = {
+    // Since `getCanonicalPath' fails silently, we check that we've properly
+    // resolved the symlink by comparing it to the original file path
+    val symlinkDest = link.getCanonicalPath
+    if (link.getPath != symlinkDest) {
+     Some(symlinkDest)
+    } else {
+      None
+    }
+  }
 }
