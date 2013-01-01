507c011e46ce14b76fe4f3d6072ba8c6bf6bbf80
David Zderic <dzderic@gmail.com>
2013-01-01 17:16:07 +1100

    camelCasing things

--- src/main/scala/com/dzderic/ps/LinuxProcess.scala
+++ src/main/scala/com/dzderic/ps/LinuxProcess.scala
@@ -5,20 +5,20 @@ import java.nio.file.attribute.PosixFileAttributes
 
 class LinuxProcess(val pid: Long) extends Process {
   // Path pointing to /proc/<pid>
-  private val proc_path = Paths.get(LinuxProcessFactory.PROC_MOUNT, pid.toString)
+  private val procPath = Paths.get(LinuxProcessFactory.PROC_MOUNT, pid.toString)
 
   // Get the process user/group from the /proc/<pid> directory permissions
   lazy private val attributes = {
-    Files.readAttributes[PosixFileAttributes](proc_path, classOf[PosixFileAttributes])
+    Files.readAttributes[PosixFileAttributes](procPath, classOf[PosixFileAttributes])
   }
   lazy val user = attributes.owner()
   lazy val group = attributes.group()
 
   // `cmdline' is split by null's. Change them to spaces.
-  lazy val cmdline = Util.readEntireFile(proc_path.resolve("cmdline").toFile).replace('\0', ' ').trim
+  lazy val cmdline = Util.readEntireFile(procPath.resolve("cmdline").toFile).replace('\0', ' ').trim
 
   // `exe' and `cwd' are symlinks which are only readable by the same user
   // running the process, which is why they're Option's
-  lazy val executable = Util.maybeReadProtectedLink(proc_path.resolve("exe").toFile)
-  lazy val cwd = Util.maybeReadProtectedLink(proc_path.resolve("cwd").toFile)
+  lazy val executable = Util.maybeReadProtectedLink(procPath.resolve("exe").toFile)
+  lazy val cwd = Util.maybeReadProtectedLink(procPath.resolve("cwd").toFile)
 }
