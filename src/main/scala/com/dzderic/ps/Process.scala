c57130834459a094bd04c3ca10cf365f52427f6c
David Zderic <dzderic@gmail.com>
2013-01-01 17:18:57 +1100

    Commenting properties of the `Process` trait

--- src/main/scala/com/dzderic/ps/Process.scala
+++ src/main/scala/com/dzderic/ps/Process.scala
@@ -3,13 +3,14 @@ package com.dzderic.ps
 import java.nio.file.attribute.{UserPrincipal, GroupPrincipal}
 
 trait Process {
-  val pid: Long
-  val startTime: Long
+  val pid: Long                    // the process id
 
-  val user: UserPrincipal
-  val group: GroupPrincipal
+  val runTime: Long                // time (in milliseconds) that the process has been running for
 
-  val cmdline: String
-  val executable: Option[String]
-  val cwd: Option[String]
+  val user: UserPrincipal          // the user running the process
+  val group: GroupPrincipal        // the group associated with the process
+
+  val cmdline: String              // the command-line used to launch the process
+  val executable: Option[String]   // the executable the process is executing
+  val cwd: Option[String]          // the working directory of the process
 }
